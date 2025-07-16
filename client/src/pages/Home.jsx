c3c08e924ed5bbd41b2801f22b461582dda7a42d
yasasArt <yasasjayaweera945@gmail.com>
2025-07-16 10:41:18 +0530

    Home page is done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <FeaturedDestination/>
         <ExclusiveOffers/>
         <Testimonial/>
+        <NewsLetter/>
     </>
   )
 }
