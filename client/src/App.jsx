30f7986febaadcb5d890d82b90af893692945a89
yasasArt <yasasjayaweera945@gmail.com>
2025-07-17 21:32:05 +0530

    Hotel Reg
    complete form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg/>}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />} />
