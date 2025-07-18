456d703514eeeb372f62b57c6ded5a6835b95eaa
yasasArt <yasasjayaweera945@gmail.com>
2025-07-18 20:29:09 +0530

    oo

--- server/node_modules/svix/dist/api/management.d.ts
+++
@@ -1,7 +0,0 @@
-import { SvixRequestContext } from "../request";
-import { ManagementAuthentication } from "./managementAuthentication";
-export declare class Management {
-    private readonly requestCtx;
-    constructor(requestCtx: SvixRequestContext);
-    get authentication(): ManagementAuthentication;
-}
--- server/node_modules/svix/dist/models/apiTokenExpireIn.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ApiTokenExpireIn {
-    expiry?: number;
-}
-export declare const ApiTokenExpireInSerializer: {
-    _fromJsonObject(object: any): ApiTokenExpireIn;
-    _toJsonObject(self: ApiTokenExpireIn): any;
-};
