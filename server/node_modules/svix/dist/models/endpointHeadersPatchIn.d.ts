6e67b61d1567adb977e2d2317060eca251ae68db
yasasArt <yasasjayaweera945@gmail.com>
2025-07-18 21:07:33 +0530

    ss

--- server/node_modules/svix/dist/models/endpointHeadersPatchIn.d.ts
+++
@@ -1,9 +0,0 @@
-export interface EndpointHeadersPatchIn {
-    headers: {
-        [key: string]: string;
-    };
-}
-export declare const EndpointHeadersPatchInSerializer: {
-    _fromJsonObject(object: any): EndpointHeadersPatchIn;
-    _toJsonObject(self: EndpointHeadersPatchIn): any;
-};
--- server/node_modules/svix/dist/util.d.ts
+++
@@ -1,9 +0,0 @@
-export interface PostOptions {
-    idempotencyKey?: string;
-}
-export declare class ApiException<T> extends Error {
-    code: number;
-    body: T;
-    headers: Record<string, string>;
-    constructor(code: number, body: T, headers: Headers);
-}
