2f747137a0ecaf3c3a7a921fd23a8d77f052bf14
yasasArt <yasasjayaweera945@gmail.com>
2025-07-18 17:59:05 +0530

    oo

--- server/node_modules/svix/dist/api/operationalWebhook.d.ts
+++
@@ -1,7 +0,0 @@
-import { SvixRequestContext } from "../request";
-import { OperationalWebhookEndpoint } from "./operationalWebhookEndpoint";
-export declare class OperationalWebhook {
-    private readonly requestCtx;
-    constructor(requestCtx: SvixRequestContext);
-    get endpoint(): OperationalWebhookEndpoint;
-}
--- server/node_modules/svix/dist/models/applicationTokenExpireIn.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ApplicationTokenExpireIn {
-    expiry?: number | null;
-}
-export declare const ApplicationTokenExpireInSerializer: {
-    _fromJsonObject(object: any): ApplicationTokenExpireIn;
-    _toJsonObject(self: ApplicationTokenExpireIn): any;
-};
