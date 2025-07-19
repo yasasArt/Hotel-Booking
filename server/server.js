ac903d4eef99d78e30eed6bfd46a0774bddb0d45
yasasArt <yasasjayaweera945@gmail.com>
2025-07-19 13:00:41 +0530

    kln

--- server/server.js
+++ server/server.js
@@ -22,3 +22,5 @@ app.get('/',(req, res)=> res.send("Api is working "))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Sever running on port ${PORT}`));
+
+//mkmkkkkkkkkkkkkkkkkkk
